4b9dfee7880863a956ec8d1c65944321d92e6bde
ajakeking <64387642+ajakeking@users.noreply.github.com>
2020-06-09 10:00:48 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Jake King
+ *         jake.king@huskers.unl.edu
+ * Date: 2020/06/9
+ * Hack 1.0
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jake King\n");
+  printf("Mechanical Engineering\n");
+  return 0;
+}
